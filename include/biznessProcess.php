3009787abc74ca672de07c38848ecd678ff871af
Domovoy <domovoy174@gmail.com>
2024-05-22 17:51:37 +0300

    update bizznesProcess

--- include/biznessProcess.php
+++ include/biznessProcess.php
@@ -1,6 +1,11 @@
 <?php
 
-// Запуск бизнес процесса
+    /*
+    Последние правки от Админ Netit24
+    Дата правок 22.05.2024
+    */
+
+// Запуск бизнесс процесса
 function biznessProcessStart($templateID, $documentID, $parametrs)
 {
     $result = CRest::call(
@@ -9,6 +14,31 @@ function biznessProcessStart($templateID, $documentID, $parametrs)
             'TEMPLATE_ID' =>  $templateID,
             'DOCUMENT_ID' => $documentID,
             'PARAMETERS' => $parametrs,
+        ]
+    );
+    return $result;
+}
+
+//Получение данных по запущенным БП
+function biznessProcessGetList($documentID)
+{
+    $result = CRest::call(
+        'bizproc.workflow.instances',
+        [
+            'select' => ["ID"],
+            'filter' => ["DOCUMENT_ID" => $documentID]
+        ]
+    );
+    return $result;
+}
+
+//отключение всех запущенных БП
+function biznessProcessTerminate($id_bp)
+{
+    $result = CRest::call(
+        'bizproc.workflow.terminate',
+        [
+            "ID" => $id_bp,
         ]
     );
     return $result;
